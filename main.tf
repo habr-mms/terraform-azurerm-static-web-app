a7b183ad9fa667919778beb74b5f1269b57ac989
Hauke Brandt <hauke.brandt@t-systems.com>
2022-10-31 13:46:31 +0100

    Added terraform files

---
+++ main.tf
@@ -0,0 +1,25 @@
+/**
+ * # static web app
+ *
+ * This module manages app service static site (static web app)
+ *
+*/
+resource "azurerm_static_site" "static_site" {
+  for_each = var.static_site
+
+  name = local.static_site[each.key].name == "" ? each.key : local.static_site[each.key].name
+  location = local.static_site[each.key].location
+  resource_group_name = local.static_site[each.key].resource_group_name
+  sku_tier = local.static_site[each.key].sku_tier
+  sku_size = local.static_site[each.key].sku_size
+  dynamic "identity" {
+    for_each = local.static_site[each.key].identity.type != "" ? [1] : []
+
+    content {
+      type         = local.static_site[each.key].identity.type
+      identity_ids = local.static_site[each.key].identity.identity_ids
+    }
+  }
+  
+  tags = local.static_site[each.key].tags
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "static_site" {
+  type        = any
+  default     = {}
+  description = "resource definition, default settings are defined within locals and merged with var settings"
+}
+
+locals {
+  default = {
+    # resource definition
+    static_site = {
+      name = ""
+      sku_tier = "Standard"
+      sku_size = "Standard"
+      identity = {
+        type         = ""
+        identity_ids = null
+      }
+    }
+  }
+
+  # merge all custom and default values
+  static_site = {
+    for static_site in keys(var.static_site) :
+    static_site => merge(local.default.static_site, var.static_site[static_site])
+  }
+}
